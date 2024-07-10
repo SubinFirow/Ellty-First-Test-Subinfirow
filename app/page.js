1640350df775512ad9daacfb7aad0c4347eeef2b
subin <subinfirow@gmail.com>
2024-07-10 13:36:14 +0530

    intial commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,178 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import {
+  Card,
+  Grid,
+  Typography,
+  Box,
+  Checkbox,
+  Divider,
+  Button,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function Home() {
+  const [checkedItems, setCheckedItems] = useState({
+    allPages: false,
+    page1: false,
+    page2: false,
+    page3: false,
+    page4: false,
+  });
+
+  const notify = (selectedPages) =>
+    toast(`${selectedPages.join(", ")} are selected`, { icon: "👏" });
+
+  const handleCheckboxChange = (event) => {
+    const { name, checked } = event.target;
+    if (name === "allPages") {
+      setCheckedItems({
+        allPages: checked,
+        page1: checked,
+        page2: checked,
+        page3: checked,
+        page4: checked,
+      });
+    } else {
+      setCheckedItems((prevState) => ({
+        ...prevState,
+        [name]: checked,
+        allPages:
+          checked &&
+          Object.keys(prevState)
+            .slice(1)
+            .every((key) => prevState[key]),
+      }));
+    }
+  };
+
+  const handleDoneClick = () => {
+    const selectedPages = Object.keys(checkedItems).filter(
+      (key) => checkedItems[key] && key !== "allPages"
+    );
+    notify(selectedPages);
+  };
+
+  const styles = {
+    boxStyle: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      minHeight: "100vh",
+    },
+    cardStyles: {
+      width: "370px",
+      height: "326px",
+      border: "1px",
+      borderRadius: "10px",
+      padding: "10px",
+      boxShadow: "0px 8px 15px 0px #1414141F",
+    },
+    gridStyles: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between",
+      padding: "8px, 15px, 8px, 22px",
+      paddingLeft: "15px",
+    },
+    buttonGrid: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      padding: "10px, 15px, 10px, 15px",
+      marginTop: "8%",
+    },
+    fontStyles: {
+      fontWeight: 200,
+      fontSize: "14px",
+    },
+    checkBoxStyles: {
+      "& .MuiSvgIcon-root": { fontSize: 24 },
+    },
+    buttonStyles: {
+      backgroundColor: "#FFCE22",
+      boxShadow: "none",
+      color: "#1F2128",
+      borderRadius: "4px",
+      height: "40px",
+      padding: "10px, 20px, 10px, 20px",
+      width: "340px",
+      textTransform: "none",
+      fontWeight: 200,
+      "&:hover": {
+        backgroundColor: "#FFD84D",
+        boxShadow: "none",
+      },
+    },
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+    <Box sx={styles.boxStyle}>
+      <Toaster />
+      <Card sx={styles.cardStyles}>
+        <Grid container>
+          <Grid item xs={12} style={styles.gridStyles}>
+            <Typography sx={styles.fontStyles}>All pages</Typography>
+            <Checkbox
+              name="allPages"
+              checked={checkedItems.allPages}
+              onChange={handleCheckboxChange}
+              sx={styles.checkBoxStyles}
             />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          </Grid>
+          <Grid item xs={12}>
+            <Divider variant="middle" />
+          </Grid>
+          <Grid item xs={12} style={styles.gridStyles}>
+            <Typography sx={styles.fontStyles}>Page 1</Typography>
+            <Checkbox
+              name="page1"
+              checked={checkedItems.page1}
+              onChange={handleCheckboxChange}
+              sx={styles.checkBoxStyles}
+            />
+          </Grid>
+          <Grid item xs={12} style={styles.gridStyles}>
+            <Typography sx={styles.fontStyles}>Page 2</Typography>
+            <Checkbox
+              name="page2"
+              checked={checkedItems.page2}
+              onChange={handleCheckboxChange}
+              sx={styles.checkBoxStyles}
+            />
+          </Grid>
+          <Grid item xs={12} style={styles.gridStyles}>
+            <Typography sx={styles.fontStyles}>Page 3</Typography>
+            <Checkbox
+              name="page3"
+              checked={checkedItems.page3}
+              onChange={handleCheckboxChange}
+              sx={styles.checkBoxStyles}
+            />
+          </Grid>
+          <Grid item xs={12} style={styles.gridStyles}>
+            <Typography sx={styles.fontStyles}>Page 4</Typography>
+            <Checkbox
+              name="page4"
+              checked={checkedItems.page4}
+              onChange={handleCheckboxChange}
+              sx={styles.checkBoxStyles}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Divider variant="middle" />
+          </Grid>
+          <Grid item xs={12} style={styles.buttonGrid}>
+            <Button
+              onClick={handleDoneClick}
+              variant="contained"
+              sx={styles.buttonStyles}
+            >
+              Done
+            </Button>
+          </Grid>
+        </Grid>
+      </Card>
+    </Box>
   );
 }
